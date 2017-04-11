965e8dbe9b5764f418552683200a4527a07a179c
BamBo <raksameth@gmail.com>
2017-04-11 10:53:32 +0700

    Layout Initailized..

--- SharePrefereneApp/app/src/main/java/com/example/raksa/sharepreferenceapp/MainActivity.java
+++ SharePrefereneApp/app/src/main/java/com/example/raksa/sharepreferenceapp/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.raksa.sharepreferenceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextFullName , editTextPassword;
+    TextView textViewPersonName , textViewProfession;
+    LinearLayout pageLayout;
+    Switch switchChangePageColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextFullName = (EditText) findViewById(R.id.editTextFullName);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        textViewPersonName = (TextView) findViewById(R.id.textViewPersonName);
+        textViewProfession = (TextView) findViewById(R.id.textViewProfession);
+        pageLayout = (LinearLayout) findViewById(R.id.pageLayout);
+        switchChangePageColor = (Switch) findViewById(R.id.switchChangePageColor);
+
+
+    }
+
+    public void onSaveButton(View view) {
+    }
+
+    public void onLoadButton(View view) {
+    }
+
+    public void OnOpenSecondActivityButton(View view) {
     }
 }
